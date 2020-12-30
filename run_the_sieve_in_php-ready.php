ff6674860c886013c2abc995cee11108ebd4dd85
ddamian97 <34185736+ddamian97@users.noreply.github.com>
2020-12-30 15:52:07 +0100

    Add files via upload

---
+++ run_the_sieve_in_php-ready.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('memory_limit', '-1');
+function run_the_code()
+{
+    $values = [1000000, 10000000, 20000000, 50000000, 100000000];
+
+    foreach ($values as $value) {
+        for($i = 0; $i < 5; $i ++){
+            $string = '{ time php era-ready.php ' . $value . ' 2>> time-php-err.txt ; }' . ' 2>> time-php.txt';
+            echo $string;
+            $output = shell_exec($string);
+            echo $output;
+        }
+    }
+}
+
+run_the_code();
+
